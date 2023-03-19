28ac80663aeaa1b9972bc8a88d6599f7430a08ce
Justin Murray <justin@popsql.com>
2023-03-19 15:35:29 -0600

    run mongo in dev

--- targets/dev.ts
+++ targets/dev.ts
@@ -1,5 +1,43 @@
-import { spawn } from 'build-strap';
+import {
+  buildLog,
+  dockerNetworkCreate,
+  onKillSignal,
+  spawn,
+} from 'build-strap';
+import { ensureDir } from 'fs-extra';
+import { dockerTeardown, runDbContainer } from './docker';
 
-export default async function dev() {
-  await spawn('tsx', ['server.ts'], { stdio: 'inherit' });
+export default async function dev(runDb = !process.argv.includes('--no-db')) {
+  const network = 'codenames-tdd';
+
+  let cleaning: Promise<void> | null = null;
+  const cleanupAndExit = async () => {
+    if (!cleaning) {
+      cleaning = (async () => {
+        buildLog('Process exiting... cleaning up...');
+        await dockerTeardown();
+        buildLog('Cleanup finished.');
+        process.exit();
+      })();
+    }
+    return cleaning;
+  };
+  onKillSignal(cleanupAndExit);
+
+  try {
+    if (runDb) {
+      await dockerNetworkCreate(network);
+      await ensureDir('./db/data-tdd');
+      const { port } = await runDbContainer();
+      process.env.DB_ENABLED = 'true';
+      process.env.DB_HOST = `mongodb://localhost:${port}`;
+    }
+    await spawn('tsx', ['server.ts'], {
+      env: process.env,
+      stdio: 'inherit',
+    });
+  } catch (err) {
+    console.error(err);
+    await cleanupAndExit();
+  }
 }
---
+++ targets/prerender.ts
@@ -0,0 +1,52 @@
+// Pre-render the app into static HTML.
+// run `yarn generate` and then `dist/static` can be served as a static site.
+
+import { readFile, readdir } from 'node:fs/promises';
+import { outputFile } from 'fs-extra';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { run } from 'build-strap';
+import build from './build';
+import { RenderResult } from '../src/entry-server.js';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const resolve = (p: string) => path.resolve(dirname, p);
+
+export default async function prerender() {
+  const clientUrl = process.env.CLIENT_URL;
+  if (!clientUrl) {
+    throw new Error('CLIENT_URL must be set');
+  }
+  await run(build);
+  const template = await readFile(
+    resolve('../dist/client/index.html'),
+    'utf-8',
+  );
+  // @ts-ignore
+  // eslint-disable-next-line import/extensions
+  const { render } = await import('../dist/server/entry-server.js');
+
+  // determine routes to pre-render from src/pages
+  const routesToPrerender = (await readdir(resolve('src/pages'))).map(
+    (file) => {
+      const name = file.replace(/\.tsx$/, '').toLowerCase();
+      return name === 'root' ? `/` : `/${name}`;
+    },
+  );
+
+  // pre-render each route...
+  for (const url of routesToPrerender) {
+    const context = {};
+    const renderResult: RenderResult = await render(url, context);
+    const { appHtml, appCss } = renderResult;
+
+    const html = template
+      .replace(`<!--app-html-->`, appHtml)
+      .replace(`/*app-css*/`, appCss)
+      .replace(`/*app-js*/`, `window.App={apiUrl:'${clientUrl}'}`);
+
+    const filePath = `../dist/static${url === '/' ? '/index' : url}.html`;
+    await outputFile(resolve(filePath), html);
+    console.log('pre-rendered:', filePath);
+  }
+}
