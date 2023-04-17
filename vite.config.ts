0590ed0c0478ee0eb3a625e1cc02c51f22553f01
Justin Murray <justin@popsql.com>
2023-04-17 13:41:15 -0600

    fix eslint for vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     minify: false,
   },
+  plugins: [react()],
 });
